d60023bb73e2ed1a479d5e824c59d580bb821cb2
znruss <zachary.n.russ@gmail.com>
2025-04-11 10:20:59 -0700

    Remove template/reference switcher

--- packages/ove/src/ToolBar/alignmentTool.js
+++ packages/ove/src/ToolBar/alignmentTool.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { Icon, Button, Intent, Classes } from "@blueprintjs/core";
+import { Icon, Button, Intent } from "@blueprintjs/core";
 import {
   FileUploadField,
   TextareaField,
   EditableTextField,
   CheckboxField,
-  RadioGroupField,
   wrapDialog
 } from "@teselagen/ui";
 import { reduxForm, FieldArray } from "redux-form";
 import { anyToJson } from "@teselagen/bio-parsers";
 import { flatMap } from "lodash-es";
 import uniqid from "shortid";
-import classNames from "classnames";
 import biomsa from "biomsa";
 import { getReverseComplementSequenceString } from "@teselagen/sequence-utils";
 
@@ -21,7 +19,6 @@ import { connectToEditor } from "../withEditorProps";
 import withEditorProps from "../withEditorProps";
 import { showDialog } from "../GlobalDialogUtils";
 import { compose } from "recompose";
-import { array_move } from "./array_move";
 
 export default connectToEditor(({ readOnly, toolBar = {} }) => {
   return {
@@ -94,9 +91,8 @@ class AlignmentToolDropdown extends React.Component {
 const ConnectedAlignmentToolDropdown = withEditorProps(AlignmentToolDropdown);
 
 class AlignmentTool extends React.Component {
-  state = {
-    templateSeqIndex: 0
-  };
+  state = {};
+
   sendSelectedDataToBackendForAlignment = async values => {
     const { addedSequences, revcomFlags = [] } = values;
     const {
@@ -105,27 +101,11 @@ class AlignmentTool extends React.Component {
       // createNewMismatchesList,
       upsertAlignmentRun
     } = this.props;
-    const { templateSeqIndex } = this.state;
 
-    // Create a mapping of original indices to revcomFlags before we reorder
-    const revcomFlagsMap = addedSequences.map((seq, i) => ({
-      id: seq.id || `seq-${i}`,
-      revcom: !!revcomFlags[i]
-    }));
-
-    // Move template sequence to first position
-    const addedSequencesToUse = array_move(addedSequences, templateSeqIndex, 0);
-
-    // Process sequences, applying reverse complement if 'revcom' is checked
-    const seqsToAlign = addedSequencesToUse.map((seq, index) => {
-      // Find the original revcom setting for this sequence
-      const seqId =
-        seq.id ||
-        `seq-${templateSeqIndex === index ? templateSeqIndex : index}`;
-      const revcomSetting = revcomFlagsMap.find(item => item.id === seqId);
-      const shouldReverseComplement = revcomSetting
-        ? revcomSetting.revcom
-        : false;
+    // No need to reorder - first sequence is always the template
+    const seqsToAlign = addedSequences.map((seq, index) => {
+      // Check if the revcom checkbox is checked for this sequence
+      const shouldReverseComplement = !!revcomFlags[index];
 
       if (shouldReverseComplement) {
         return {
@@ -200,7 +180,7 @@ class AlignmentTool extends React.Component {
     });
     onChange([]);
   };
-  renderAddSequence = ({ fields, templateSeqIndex }) => {
+  renderAddSequence = ({ fields }) => {
     const { handleSubmit } = this.props;
 
     const sequencesToAlign = fields.getAll() || [];
@@ -239,35 +219,11 @@ class AlignmentTool extends React.Component {
                   key={index}
                 >
                   <div style={{ display: "flex", alignItems: "center" }}>
-                    <RadioGroupField
-                      name="templateSequence"
-                      value={templateSeqIndex === index ? index.toString() : ""}
-                      onChange={() => {
-                        this.setState({
-                          templateSeqIndex: index
-                        });
-                      }}
-                      options={[
-                        { value: index.toString(), label: addedSeq.name }
-                      ]}
-                      inline
-                      style={{ margin: 0, marginRight: "5px" }}
-                    />
+                    {addedSeq.name}{" "}
                     <span style={{ fontSize: 10 }}>
                       {" "}
                       ({addedSeq.sequence.length} bps)
                     </span>
-                    {index === templateSeqIndex && (
-                      <div
-                        className={classNames(
-                          Classes.TAG,
-                          Classes.ROUND,
-                          Classes.INTENT_PRIMARY
-                        )}
-                      >
-                        template
-                      </div>
-                    )}
                   </div>
                   <div
                     style={{
@@ -286,9 +242,6 @@ class AlignmentTool extends React.Component {
                         e.stopPropagation();
                         e.preventDefault();
                         fields.remove(index);
-                        if (index === templateSeqIndex) {
-                          this.setState({ templateSeqIndex: 0 });
-                        }
                       }}
                     >
                       Remove
@@ -315,7 +268,6 @@ class AlignmentTool extends React.Component {
 
   render() {
     const { selectFromSequenceLibraryHook } = this.props;
-    const { templateSeqIndex } = this.state;
     return (
       <div style={{ padding: 20 }} className="veAlignmentTool">
         <h6>Upload files you'd like to align (.ab1, .fasta, .gb) </h6>
@@ -328,11 +280,7 @@ class AlignmentTool extends React.Component {
           <h6>Or Select from your sequence library </h6>
         )}
 
-        <FieldArray
-          name="addedSequences"
-          templateSeqIndex={templateSeqIndex}
-          component={this.renderAddSequence}
-        />
+        <FieldArray name="addedSequences" component={this.renderAddSequence} />
       </div>
     );
   }
